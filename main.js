c0074650729594b7ba72a7f5a836faaaa274c169
jorik004 <57297036+jorik004@users.noreply.github.com>
2022-02-06 16:22:20 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
-
-
-
-let bgimg = document.querySelector('.bgimg');
-let login = document.querySelector(".pashalka");
-let wrapper = document.querySelector(".wrapper");
-let body = document.querySelector('body');
-
-
-login.onclick = function(){
-  bgimg.style.display = 'block';
-  bgimg.style.backgroundColor = 'black';
-  wrapper.style.display = 'none';
-  login.style.display = 'none';
-  body.style.backgroundColor = 'black';
-}
+pashalka = document.querySelector('.pashalka');
+container = document.querySelector('.container');
+pashalkaAlan = document.querySelector('.pashalkaAlan');
+pashalka.addEventListener('click', function(){
+  container.style.display = 'none';
+  pashalkaAlan.style.display = 'block';
+});
