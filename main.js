f10e15742783d5a280db48f8f5f8fcfbab06b2a2
jorik004 <57297036+jorik004@users.noreply.github.com>
2022-02-06 17:37:15 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+localStorage.getItem('_users');
+
 pashalka = document.querySelector('.pashalka');
 container = document.querySelector('.container');
 pashalkaAlan = document.querySelector('.pashalkaAlan');
+users = document.querySelector('.users');
+_users = 1;
+console.log(_users);
+
+
 pashalka.addEventListener('click', function(){
   container.style.display = 'none';
   pashalkaAlan.style.display = 'block';
+  _users++;
+  users.innerText = _users;
+  localStorage.setItem('_users', _users);
 });
